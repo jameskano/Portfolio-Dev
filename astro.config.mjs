933394fa136c21ecaffd39ef36d6431eb50272a5
Jaime <jaimecanovasdesign@gmail.com>
2026-04-10 17:56:41 +0200

    Improve portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://www.jaimecanovas.xyz/',
+  devToolbar: {
+    enabled: false,
+  },
 })
